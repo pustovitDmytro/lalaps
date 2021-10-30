11c8dcd9bebbec09a394f4dcd3190c2e0a08ae99
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-10-31 02:19:57 +0300

    Chore: fixes lint

--- src/platforms/Base.js
+++ src/platforms/Base.js
@@ -62,7 +62,7 @@ export class BaseRepo {
     async handleValidConfig(confRes, onboardingPR) {
         if (onboardingPR?.isOpen) {
             const pr = await this.api.autoclosePR(this.repo, onboardingPR, {
-                body : await templates.addText(onboardingPR.body, 'config_found.md')
+                body : await templates.addText(onboardingPR.body, 'onboarding/config_found.md')
             });
 
             return new platfRes.ONBOARDING_PR_CLOSED(pr);
@@ -123,64 +123,80 @@ export class BaseRepo {
         }
     }
 
+    async handleFixFound(advisory, config, res, { targetPr, targetBranch }) {
+        const Advisory = advisory.constructor;
+        const { stats } = advisory.analizeReport(res.report);
+        const text = await templates.text(advisory.getPrTemplate(res), { stats });
+
+        if (targetPr?.isOpen) {
+            const prev = await templates.extract(targetPr.body, 'Lalaps.description');
+            const next = await templates.extract(text, 'Lalaps.description');
+
+            if (prev !== next) {
+                const pr = await this.api.updatePR(this.repo, targetPr, { body: text });
+
+                return new platfRes.FIX_PR_OPEN(pr);
+            }
+
+            if (config.automerge) {
+                const checks = await this.api.getChecks(this.repo, targetPr);
+
+                if (checks.every(c => c.isSucceeded)) {
+                    const m = await this.api.automergePR(this.repo, targetPr);
+
+                    return new platfRes.FIX_PR_MERGED(m);
+                }
+            }
+
+            return new platfRes.FIX_PR_OPEN(targetPr);
+        }
+
+        const messsage = advisory.getCommitMessage(res);
+
+        await this.git.uploadFiles(targetBranch, Advisory.Files, messsage);
+
+        if (!targetPr?.isOpen) {
+            const pr = await this.api.createPR(this.repo, {
+                title  : messsage,
+                body   : text,
+                head   : targetBranch,
+                base   : this.repo.branch,
+                labels : config.labels
+            });
+
+            return new platfRes.FIX_PR_OPEN(pr);
+        }
+    }
+
     async runAdvisory(config) {
         const Advisory = advisories[config.advisory];
         const advisory = new Advisory({ ...config, folder: this.git.folder });
         const res = await advisory.run();
+        const targetBranch = advisory.getTragetBranch(res);
+        const concurentBranch = advisory.getConcurentBranch(res);
 
-        if (res instanceof advRes.PARTIAL_FIX || res instanceof advRes.FULL_FIX) {
-            const targetBranch = advisory.getTragetBranch(res);
-            const concurentBranch = advisory.getConcurentBranch(res);
+        const [ targetPr, concurentPr ] = await Promise.all([
+            this.api.findPR(this.repo, targetBranch),
+            this.api.findPR(this.repo, concurentBranch)
+        ]);
 
-            const [ targetPr, concurentPr ] = await Promise.all([
-                this.api.findPR(this.repo, targetBranch),
-                this.api.findPR(this.repo, concurentBranch)
-            ]);
+        if (res instanceof advRes.NOT_VULNERABLE) {
+            await Promise.all(
+                [ targetPr, concurentPr ]
+                    .filter(pr => pr?.isOpen)
+                    .map(async pr => {
+                        const body = await templates.addText(pr.body, 'already_fixed.md');
 
-            const { stats } = advisory.analizeReport(res.report);
-            const text = await templates.text(advisory.getPrTemplate(res), { stats });
+                        return this.api.autoclosePR(this.repo, pr, { body });
+                    })
+            );
 
-            if (targetPr?.isOpen) {
-                const prev = await templates.extract(targetPr.body, 'Lalaps.description');
-                const next = await templates.extract(text, 'Lalaps.description');
-
-                if (prev !== next) {
-                    const pr = await this.api.updatePR(this.repo, targetPr, { body: text });
-
-                    return new platfRes.FIX_PR_OPEN(pr);
-                }
-
-                if (config.automerge) {
-                    const checks = await this.api.getChecks(this.repo, targetPr);
-
-                    if (checks.every(c => c.isSucceeded)) {
-                        const m = await this.api.automergePR(this.repo, targetPr);
-
-                        return new platfRes.FIX_PR_MERGED(m);
-                    }
-                }
-
-                return new platfRes.FIX_PR_OPEN(targetPr);
-            }
-
-            const messsage = advisory.getCommitMessage(res);
-
-            await this.git.uploadFiles(targetBranch, Advisory.Files, messsage);
-
-            if (!targetPr?.isOpen) {
-                const pr = await this.api.createPR(this.repo, {
-                    title  : messsage,
-                    body   : text,
-                    head   : targetBranch,
-                    base   : this.repo.branch,
-                    labels : config.labels
-                });
-
-                return new platfRes.FIX_PR_OPEN(pr);
-            }
+            return res;
         }
 
-        return res;
+        if (res instanceof advRes.PARTIAL_FIX || res instanceof advRes.FULL_FIX) {
+            return this.handleFixFound(advisory, config, res, { targetPr, concurentPr, targetBranch, concurentBranch });
+        }
     }
 }
 
---
+++ templates/already_fixed.md
@@ -0,0 +1,3 @@
+<!-- Lalaps.close-reason:start -->
+All vulnerabilities fixed in default branch.
+<!-- Lalaps.close-reason:end -->
