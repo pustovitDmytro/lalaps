5f0cc1c38e7dfbd72b16bdb65839b539f4bc84d1
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-11-21 19:08:23 +0200

    Fix: return attachment

--- src/web.js
+++ src/web.js
@@ -75,6 +75,7 @@ app.use('/admin/info', checkBasicAuth, renderInfo);
 
 function renderFile(file) {
     app.use(`/${file.fileName}`, (req, res) => {
+        res.attachment(file.fileName);
         res.type('txt');
         res.send(file.content);
     });
