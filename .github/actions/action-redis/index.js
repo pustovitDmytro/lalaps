244dbaaa3f5c31788c0b1888599797004475a699
Dmytro Pustovit <dipustovit@gmail.com>
2022-02-10 18:59:09 +0200

    Chore: print process.platform

--- .github/actions/action-redis/index.js
+++ .github/actions/action-redis/index.js
@@ -11,7 +11,8 @@ async function run(){
       // Get the JSON webhook payload for the event that triggered the workflow
       const payload = JSON.stringify(github.context.payload, undefined, 2)
       console.log(`The event payload: ${payload}`);
-
+      
+      console.log('process.platform: ', process.platform);
       if(process.platform=='win32'){
           console.log('Windows');
           return;
